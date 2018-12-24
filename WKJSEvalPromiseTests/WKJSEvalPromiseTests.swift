32a1d349d5df8320b08be07eab8ea41b5af0846d
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2018-12-25 01:16:46 +0200

    improve unit test

--- WKJSEvalPromiseTests/WKJSEvalPromiseTests.swift
+++ WKJSEvalPromiseTests/WKJSEvalPromiseTests.swift
@@ -34,19 +34,32 @@ class WKJSEvalPromiseTests: XCTestCase {
     }
 
     func testSerializingLongExecution() {
+        var count = 3
+        
         WKJSEvalPromise.firstly(jsEvaluator: jsEvaluator) { () -> String in
             return "f1()"
         }
         .then { (result, error) -> String in
             XCTAssertEqual(result as! String, "f1()")
+            
+            count -= 1
+            XCTAssertEqual(count, 2)
+            
             return "f2()"
         }
         .then { (result, error) -> String in
             XCTAssertEqual(result as! String, "f2()")
+            
+            count -= 1
+            XCTAssertEqual(count, 1)
+            
             return "f3()"
         }
         .finally { (result, error) in
             XCTAssertEqual(result as! String, "f3()")
+            
+            count -= 1
+            XCTAssertEqual(count, 0)
         }
         
         jsEvaluator.queue.sync {}
